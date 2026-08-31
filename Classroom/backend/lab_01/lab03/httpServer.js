1a0ee65c65fc353d49e11c4070f6ea562cb739ea
= <Arpittechbdh13@gmail.com>
2026-08-31 16:02:16 +0530

    new update

--- Classroom/backend/lab_01/lab03/httpServer.js
+++ Classroom/backend/lab_01/lab03/httpServer.js
@@ -1,5 +1,5 @@
 import http from "http";
-const userdata =[{id:"theright" , name: "helllo", }]
+const userdata = [{ id: "theright", name: "helllo" }];
 const server = http.createServer((req, res) => {
   //   res.statusCode = 201;
   //   res.setHeader("Content-type", "text/plain");
@@ -10,17 +10,12 @@ const server = http.createServer((req, res) => {
   const method = req.method;
   if (url == "/msg" && method == "GET") {
     res.end("this is welcome message from server");
-    
-  }
-  else if(url=="/sys" && method =='GET'){
+  } else if (url == "/sys" && method == "GET") {
     res.end("This is system information ");
-  }
-
-  else if(url =="/data" && method =="GET"){
-    res.end(JSON.stru)
+  } else if (url == "/data" && method == "GET") {
+    res.end(JSON.stringify(userdata));
   }
 });
 server.listen(3000, () => {
   console.log("server is running on port number 3000");
 });
-
