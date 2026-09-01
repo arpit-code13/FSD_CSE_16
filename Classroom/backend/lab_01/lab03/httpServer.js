63beccf6bbd251d7e2b6cf8e77d5edd5130e5d0c
= <Arpittechbdh13@gmail.com>
2026-09-01 11:09:24 +0530

    update

--- Classroom/backend/lab_01/lab03/httpServer.js
+++ Classroom/backend/lab_01/lab03/httpServer.js
@@ -1,21 +1,33 @@
-import http from "http";
-const userdata = [{ id: "theright", name: "helllo" }];
-const server = http.createServer((req, res) => {
-  //   res.statusCode = 201;
-  //   res.setHeader("Content-type", "text/plain");
-  //   res.end("hello server");
+import http from 'http';
 
-  /////////////////////////
-  const url = req.url;
-  const method = req.method;
-  if (url == "/msg" && method == "GET") {
-    res.end("this is welcome message from server");
-  } else if (url == "/sys" && method == "GET") {
-    res.end("This is system information ");
-  } else if (url == "/data" && method == "GET") {
-    res.end(JSON.stringify(userdata));
-  }
+const userdata = {
+    name: 'arpit',
+    age: 20
+};
+
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/plain');
+    const url = req.url;
+    const method = req.method;
+    if (url === '/' && method === 'GET') {
+        res.statusCode = 200;
+        res.end('Welcome to the Home Page');
+
+    }
+    else if (url === '/sys' && method === 'GET') {
+        res.statusCode = 201;
+        res.end('Welcome to the System Page');
+    }
+    else if (url === '/data' && method === 'GET') {
+        res.statusCode = 200;
+        res.end(JSON.stringify(userdata));
+    }
+    else {
+        res.statusCode = 404;
+        res.end('Page not found');
+    }
 });
+
 server.listen(3000, () => {
-  console.log("server is running on port number 3000");
+    console.log('Server running on http://localhost:3000');
 });
